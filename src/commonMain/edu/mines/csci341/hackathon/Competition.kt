54eb86229f3ffc9b9dafd74d4daa6a00d7947032
Lucas Kitaev <lkitaev@mymail.mines.edu>
2021-05-30 14:41:28 -0600

    Update models

--- src/commonMain/edu/mines/csci341/hackathon/Competition.kt
+++ src/commonMain/edu/mines/csci341/hackathon/Competition.kt
@@ -13,9 +13,9 @@ data class Competition(
 	val title: String,
 	val description: String,
 	val contents: String,
+	val isActive: Boolean = false,
 	val created: LocalDate = Clock.System.now()
 		.toLocalDateTime(TimeZone.currentSystemDefault()).date,
-	val isActive: Boolean = true,
 ) {
 	var expectedResults: List<Pair<String, String>> = listOf()
 	var participants: List<User> = listOf()
@@ -29,9 +29,9 @@ data class Competition(
 
 	companion object {
 		val comps = listOf(
-			Competition(1, "Test 1", "Test Description", "Test Contents", "2020-08-29".toLocalDate()),
-			Competition(2, "Test 2", "Test Description", "Test Contents", "2021-01-29".toLocalDate()),
-			Competition(3, "Test 3", "Test Description", "Test Contents", "2021-05-29".toLocalDate(), false),
+			Competition(1, "Test 1", "Test Description 1", "Test Contents 1", true, "2020-08-29".toLocalDate()),
+			Competition(2, "Test 2", "Test Description 2", "Test Contents 2", true, "2021-01-29".toLocalDate()),
+			Competition(3, "Test 3", "Test Description 3", "Test Contents 3", false, "2021-05-29".toLocalDate()),
 		)
 		init {
 			comps[0].apply {
--- src/commonMain/edu/mines/csci341/hackathon/Submission.kt
+++ src/commonMain/edu/mines/csci341/hackathon/Submission.kt
@@ -17,9 +17,9 @@ data class Submission(
 
 	companion object {
 		val subs = listOf(
-			Submission(1, Competition.comps[0], "Test Contents"),
-			Submission(2, Competition.comps[1], "Test Contents"),
-			Submission(3, Competition.comps[2], "Test Contents"),
+			Submission(1, Competition.comps[0], "Test Submission 1"),
+			Submission(2, Competition.comps[1], "Test Submission 2"),
+			Submission(3, Competition.comps[2], "Test Submission 3"),
 		)
 		init {
 			subs[0].results = listOf("1" to "2")
