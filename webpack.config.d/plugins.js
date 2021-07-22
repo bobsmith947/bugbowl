ecd142850a9e53432babf6ee3f46692c05b42b76
Lucas Kitaev <lkitaev@mymail.mines.edu>
2021-07-22 14:37:48 -0600

    Added brotli compression in addition to gzip

--- webpack.config.d/plugins.js
+++ webpack.config.d/plugins.js
@@ -1,5 +1,20 @@
 const CompressionPlugin = require("compression-webpack-plugin");
+const zlib = require("zlib");
 
 config.plugins.push(
-	new CompressionPlugin()
+	new CompressionPlugin({
+		filename: "[path][base].br",
+		algorithm: "brotliCompress",
+		test: /\.js$/,
+		compressionOptions: {
+			params: {
+				[zlib.constants.BROTLI_PARAM_QUALITY]: 11
+			}
+		}
+	}),
+	new CompressionPlugin({
+		filename: "[path][base].gz",
+		algorithm: "gzip",
+		test: /\.js$/
+	})
 );
